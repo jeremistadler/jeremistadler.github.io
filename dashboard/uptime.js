69f7ed2d53833df2af84c910b982385a7b41bde7
Jeremi Stadler <stadler.jeremi@gmail.com>
2015-06-25 18:08:19 +0200

    remove uptime graph

--- dashboard/uptime.js
+++ dashboard/uptime.js
@@ -1,3 +1,30 @@
+var drawResponseGraph = function (data) {
+    var chart = d3
+        .select("#responseChart")
+        .append('svg')
+        .attr('width', 300)
+        .attr('height', 150)
+        .append("g")
+        .attr("transform", "translate(20,20)");
+    var color = d3.scale.linear()
+        .domain([100, 5000])
+        .range(['green', 'red']);
+    var verticalScale = d3.scale.linear()
+        .domain([100, 5000])
+        .range([130, 10]);
+    var minTime = d3.min(data, function (d) { return d.datetime; });
+    var maxTime = d3.max(data, function (d) { return d.datetime; });
+    var timeScale = d3.time.scale()
+        .domain([minTime, maxTime])
+        .range([0, 270]);
+    chart
+        .selectAll("div")
+        .data(data)
+        .enter().append('circle')
+        .attr('r', 3)
+        .attr('fill', function (d) { return color(d.value); })
+        .attr('transform', function (d) { return 'translate(' + timeScale(d.datetime) + ',' + verticalScale(d.value) + ')'; });
+};
 var updateMonitorGraph = function () {
     var apiKey = 'm776903669-4c9d4e88a25c513045a2e656';
     var url = 'https://api.uptimerobot.com/getMonitors?apiKey=' + apiKey + '&logs=1&responseTimes=1&format=json&noJsonCallback=1';
@@ -5,51 +32,10 @@ var updateMonitorGraph = function () {
         if (error)
             return console.warn(error);
         var format = d3.time.format("%m/%d/%y %H:%M:%S");
-        var color = d3.scale.ordinal()
-            .domain([1, 2, 98, 99])
-            .range(['red', 'green', 'orange', 'gray']);
-        var chart = d3
-            .select("#uptimeChart")
-            .append('svg')
-            .attr('width', 300)
-            .attr('height', 150)
-            .append("g")
-            .attr("transform", "translate(20,20)");
-        var i = 0;
-        var bar = chart
-            .selectAll("div")
-            .data(json.monitors.monitor[0].log)
-            .enter().append('rect')
-            .attr('width', 10)
-            .attr('height', 10)
-            .attr('fill', function (d) { return color(d.type); })
-            .attr('transform', function (d) { return 'translate(' + (i++) * 10 + ',0)'; });
-        var chart = d3
-            .select("#responseChart")
-            .append('svg')
-            .attr('width', 300)
-            .attr('height', 150)
-            .append("g")
-            .attr("transform", "translate(20,20)");
-        var color = d3.scale.linear()
-            .domain([100, 5000])
-            .range(['green', 'red']);
-        var verticalScale = d3.scale.linear()
-            .domain([100, 5000])
-            .range([130, 10]);
-        var minTime = d3.min(json.monitors.monitor[0].responsetime, function (d) { return format.parse(d.datetime); });
-        var maxTime = d3.max(json.monitors.monitor[0].responsetime, function (d) { return format.parse(d.datetime); });
-        var timeScale = d3.time.scale()
-            .domain([minTime, maxTime])
-            .range([0, 270]);
-        var i = 0;
-        var bar = chart
-            .selectAll("div")
-            .data(json.monitors.monitor[0].responsetime)
-            .enter().append('circle')
-            .attr('r', 3)
-            .attr('fill', function (d) { return color(d.value); })
-            .attr('transform', function (d) { return 'translate(' + timeScale(format.parse(d.datetime)) + ',' + verticalScale(d.value) + ')'; });
+        var responseTimes = json.monitors.monitor[0].responsetime;
+        for (var i = 0; i < responseTimes.length; i++)
+            responseTimes[i].datetime = format.parse(responseTimes[i].datetime);
+        drawResponseGraph(responseTimes);
     });
 };
 updateMonitorGraph();
--- dashboard/uptime.ts
+++ dashboard/uptime.ts
@@ -1,89 +1,56 @@
 
-var updateMonitorGraph = function(){
-  var apiKey = 'm776903669-4c9d4e88a25c513045a2e656';
-
-  var url = 'https://api.uptimerobot.com/getMonitors?apiKey=' + apiKey + '&logs=1&responseTimes=1&format=json&noJsonCallback=1';
-  d3.json(url, function(error, json) {
-  if (error) return console.warn(error);
-
-  var format = d3.time.format("%m/%d/%y %H:%M:%S");
-  var color = d3.scale.ordinal()
-                .domain([1, 2, 98, 99])
-                .range(['red', 'green', 'orange', 'gray']);
+var drawResponseGraph = function(data) {
 
   var chart = d3
-  .select("#uptimeChart")
-  .append('svg')
-  .attr('width', 300)
-  .attr('height', 150)
-  .append("g")
-  .attr("transform", "translate(20,20)")
-  ;
+    .select("#responseChart")
+    .append('svg')
+    .attr('width', 300)
+    .attr('height', 150)
+    .append("g")
+    .attr("transform", "translate(20,20)")
+    ;
 
 
-      var minTime = d3.min(json.monitors.monitor[0].log, d => format.parse(d.datetime));
-      var maxTime = d3.max(json.monitors.monitor[0].log, d => format.parse(d.datetime));
+  var color = d3.scale.linear()
+    .domain([100, 5000])
+    .range(['green', 'red']);
 
-  var uptimeDateScale = d3.time.scale()
-                          .domain([minTime, maxTime])
-                          .range([10, 200]);
+  var verticalScale = d3.scale.linear()
+    .domain([100, 5000])
+    .range([130, 10]);
 
-  var widthScale = d3.scale.linear()
-                     .domain([0, maxTime.getTime() - minTime.getTime()])
-                     .range([20, 50]);
+  var minTime = d3.min(data, d => d.datetime);
+  var maxTime = d3.max(data, d => d.datetime);
 
-var logs = <any[]>json.monitors.monitor[0].log;
-logs.sort(f => format.parse(f.datetime).getTime());
+  var timeScale = d3.time.scale()
+    .domain([minTime, maxTime])
+    .range([0, 270]);
 
+  chart
+    .selectAll("div")
+    .data(data)
+    .enter().append('circle')
+    .attr('r', 3)
+    .attr('fill', d => color(d.value))
+    .attr('transform', d => 'translate(' + timeScale(d.datetime) + ',' + verticalScale(d.value) + ')');
 
-   chart
-      .selectAll("div")
-      .data(json.monitors.monitor[0].log)
-      .enter().append('rect')
-      .attr('width', (d, i) => {
-        var nextTime = (i > logs.length - 2 ? maxTime : format.parse(logs[i + 1].datetime)).getTime()
-        var currentTime = format.parse(d.datetime).getTime();
-        return widthScale(nextTime - currentTime);
-      })
-      .attr('height', 10)
-      .attr('fill', d => color(d.type));
+}
 
+var updateMonitorGraph = function() {
+  var apiKey = 'm776903669-4c9d4e88a25c513045a2e656';
+  var url = 'https://api.uptimerobot.com/getMonitors?apiKey=' + apiKey + '&logs=1&responseTimes=1&format=json&noJsonCallback=1';
 
-var chart = d3
-      .select("#responseChart")
-      .append('svg')
-      .attr('width', 300)
-      .attr('height', 150)
-      .append("g")
-      .attr("transform", "translate(20,20)")
-      ;
+  d3.json(url, function(error, json) {
+    if (error) return console.warn(error);
 
+    var format = d3.time.format("%m/%d/%y %H:%M:%S");
+    var responseTimes = <any[]>json.monitors.monitor[0].responsetime;
 
-    var color = d3.scale.linear()
-                  .domain([100, 5000])
-                  .range(['green', 'red']);
+    for (let i = 0; i < responseTimes.length; i++)
+      responseTimes[i].datetime = format.parse(responseTimes[i].datetime);
 
-    var verticalScale = d3.scale.linear()
-                  .domain([100, 5000])
-                  .range([130, 10]);
-
-    var minTime = d3.min(json.monitors.monitor[0].responsetime, d => format.parse(d.datetime));
-    var maxTime = d3.max(json.monitors.monitor[0].responsetime, d => format.parse(d.datetime));
-
-    var timeScale = d3.time.scale()
-                      .domain([minTime, maxTime])
-                      .range([0, 270]);
-
-    var i = 0;
-        var bar = chart
-          .selectAll("div")
-          .data(json.monitors.monitor[0].responsetime)
-          .enter().append('circle')
-          .attr('r', 3)
-          .attr('fill', d => color(d.value))
-          .attr('transform', d => 'translate(' + timeScale(format.parse(d.datetime)) + ',' + verticalScale(d.value) + ')');
-
-});
+    drawResponseGraph(responseTimes);
+  });
 }
 
 updateMonitorGraph();
